d093455624bca86d113be49a4b911f99f7c87ae2
Sean LAIDLAW <seanlaidlaw95@gmail.com>
2023-02-28 10:55:20 +0000

    add automatic download of captured images as a Zip archive

--- ZoomSlideCapture.js
+++ ZoomSlideCapture.js
@@ -4,9 +4,7 @@
 // @version      0.1
 // @description  Script to capture Screen Share as PNG data every 1s
 // @author       Sean Laidlaw
-// @require https://rawgit.com/kriskowal/q/v1/q.js
-// @require https://code.jquery.com/jquery-3.6.3.min.js
-// @require https://raw.github.com/tantaman/LargeLocalStorage/master/dist/LargeLocalStorage.min.js
+// @require https://raw.githubusercontent.com/ericvergnaud/jszip/sync-methods/dist/jszip.js
 // @match        https://sanger.zoom.us/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant       unsafeWindow
@@ -18,19 +16,34 @@ var intervals = [];
 var isCapturing = false
 
 function toggleCapture() {
+	// this is the canvas that Zoom uses to display the screen share
 	let screen_share = document.getElementById('sharee-container-canvas')
 
 	if (isCapturing) {
 		isCapturing = false
-		intervals.forEach(clearInterval);
+		intervals.forEach(clearInterval); // stop capturing screen every 2 second
 		document.getElementById('startCaptureBtn').innerText = "Start Capture"
 
-		for (let i = 0; i < images.length; i++) {
-			downloadFile(images[i])
-		}
+		// initiate zip file to contain the captured images
+		var zip = new JSZip();
+		var zipped = zip.sync(function () { // sync method to avoid async issues in Tapermonkey
+			var img = zip.folder("images");
+			for (let i = 0; i < images.length; i++) {
+				var idx = images[i].indexOf('base64,') + 'base64,'.length;
+				var content = images[i].substring(idx);
+				img.file(i + ".png", content, {base64: true});
+			}
+			var data = null;
+			zip.generateAsync({type: "base64"})
+				.then(function (content) {
+					data = content;
+					downloadFile("data:application/zip;base64," + content)
+				});
+		});
 	} else {
 		isCapturing = true
 		const interval = setInterval(function () {
+			// capture state of canvas and append it to the images array
 			let capture = screen_share.toDataURL('image/png', 1.0);
 			if (!(images.includes(capture))) {
 				images.push(capture)
@@ -43,6 +56,8 @@ function toggleCapture() {
 
 }
 
+// this creates a button linking to a file and then clicks it to download it
+// it avoids the error caused by browser blocking redirect to a file
 function downloadFile(filePath) {
 	var link = document.createElement('a');
 	link.href = filePath;
@@ -52,10 +67,11 @@ function downloadFile(filePath) {
 
 
 (function () {
-
 	// this uses a manual 5000ms timer before running this code, due to the webpage dynamically loading.
 	// without this it runs instantly before any of the targeted elements appear
 	setTimeout(function () {
+
+		//--- Add a button to the Zoom toolbar to toggle capture of screen share.
 		var shareBtn = document.createElement('div');
 		shareBtn.innerHTML = '<button id="startCaptureBtn" type="button">Start Capture</button>';
 		shareBtn.setAttribute('class', 'footer-button-base__button')
